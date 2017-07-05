7ab9937c9f51e6c6fbe52e514f78ef347c13db77
ben smith <benjaminsmith1981@gmail.com>
2017-07-05 09:32:07 +0200

    created image rotated extnsions

--- iGenFamilyTree/iGenFamilyTree/Model.swift
+++ iGenFamilyTree/iGenFamilyTree/Model.swift
@@ -39,7 +39,7 @@ enum cellState {
         switch self {
             
         case .son:
-            return imageRotatedByDegrees(oldImage: #imageLiteral(resourceName: "paths-male-1-connector"), deg: 180)
+            return #imageLiteral(resourceName: "paths-male-1-connector").imageRotatedByDegrees(deg: 180)
         case .daughter:
             return #imageLiteral(resourceName: "paths-TEST-female-daughter")
         case .malePatient:
@@ -47,57 +47,36 @@ enum cellState {
         case .femalePatient:
             return #imageLiteral(resourceName: "paths-female-corner-2-connectors")
         case .maleSpouse:
-            return imageRotatedByDegrees(oldImage: #imageLiteral(resourceName: "paths-male-corner-2-connectors"), deg: 90)
+            return #imageLiteral(resourceName: "paths-male-corner-2-connectors").imageRotatedByDegrees(deg: 90)
         case .femaleSpouse:
-            return imageRotatedByDegrees(oldImage: #imageLiteral(resourceName: "paths-female-1-connector"), deg: -90)
+            return #imageLiteral(resourceName: "paths-female-1-connector").imageRotatedByDegrees(deg: -90)
         case .aunt:
-            return imageRotatedByDegrees(oldImage: #imageLiteral(resourceName: "paths-female-1-connector"), deg: 180)
+            return #imageLiteral(resourceName: "paths-female-1-connector").imageRotatedByDegrees(deg: 180)
         case .uncle:
-            return imageRotatedByDegrees(oldImage: #imageLiteral(resourceName: "paths-male-1-connector"), deg: 180)
+            return #imageLiteral(resourceName: "paths-male-1-connector").imageRotatedByDegrees(deg: 180)
         case .father:
-            return imageRotatedByDegrees(oldImage: #imageLiteral(resourceName: "paths-male-1-connector"), deg: 90)
+            return #imageLiteral(resourceName: "paths-male-1-connector").imageRotatedByDegrees(deg: 90)
         case .mother:
-            return imageRotatedByDegrees(oldImage: #imageLiteral(resourceName: "paths-female-1-connector"), deg: -90)
+            return #imageLiteral(resourceName: "paths-female-1-connector").imageRotatedByDegrees(deg: -90)
         case .spouseConnector:
             return #imageLiteral(resourceName: "paths-T-3-connectors")
         case .patientParentConnector:
-            return imageRotatedByDegrees(oldImage: #imageLiteral(resourceName: "paths-T-3-connectors"), deg: -90)
+            return #imageLiteral(resourceName: "paths-T-3-connectors").imageRotatedByDegrees(deg: -90)
         case .cornerLeftBottom:
-            return imageRotatedByDegrees(oldImage: #imageLiteral(resourceName: "paths-corner"), deg: 90)
+            return #imageLiteral(resourceName: "paths-corner").imageRotatedByDegrees(deg: 90)
         case .cornerLeftTop:
-            return imageRotatedByDegrees(oldImage: #imageLiteral(resourceName: "paths-corner"), deg: 180)
+            return #imageLiteral(resourceName: "paths-corner").imageRotatedByDegrees(deg: 180)
         case .cornerRightBottom:
             return #imageLiteral(resourceName: "paths-corner")
         case .cornerRightTop:
-            return imageRotatedByDegrees(oldImage: #imageLiteral(resourceName: "paths-corner"), deg: -90)
+            return #imageLiteral(resourceName: "paths-corner").imageRotatedByDegrees(deg: -90)
         case .straightHorizontal:
             return #imageLiteral(resourceName: "paths-straight")
         case .straightVertical:
-            return imageRotatedByDegrees(oldImage: #imageLiteral(resourceName: "paths-straight"), deg: 90)
+            return #imageLiteral(resourceName: "paths-straight").imageRotatedByDegrees(deg: 90)
         case .emptyCell:
             return UIImage()
         }
-    }
-
-    func imageRotatedByDegrees(oldImage: UIImage, deg degrees: CGFloat) -> UIImage {
-        //Calculate the size of the rotated view's containing box for our drawing space
-        let rotatedViewBox: UIView = UIView(frame: CGRect(x: 0, y: 0, width: oldImage.size.width, height: oldImage.size.height))
-        let t: CGAffineTransform = CGAffineTransform(rotationAngle: degrees * CGFloat.pi / 180)
-        rotatedViewBox.transform = t
-        let rotatedSize: CGSize = rotatedViewBox.frame.size
-        //Create the bitmap context
-        UIGraphicsBeginImageContext(rotatedSize)
-        let bitmap: CGContext = UIGraphicsGetCurrentContext()!
-        //Move the origin to the middle of the image so we will rotate and scale around the center.
-        bitmap.translateBy(x: rotatedSize.width / 2, y: rotatedSize.height / 2)
-        //Rotate the image context
-        bitmap.rotate(by: (degrees * CGFloat.pi / 180))
-        //Now, draw the rotated/scaled image into the context
-        bitmap.scaleBy(x: 1.0, y: -1.0)
-        bitmap.draw(oldImage.cgImage!, in: CGRect(x: -oldImage.size.width / 2, y: -oldImage.size.height / 2, width: oldImage.size.width, height: oldImage.size.height))
-        let newImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-        return newImage
     }
     
     
