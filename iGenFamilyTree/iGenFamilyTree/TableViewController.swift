acd1b4fa70ae53b9a26c5a4c3117143b48c1dbb5
Achid Farooq <achidfarooq@gmail.com>
2017-07-05 18:32:09 +0200

    enum moved

--- iGenFamilyTree/iGenFamilyTree/TableViewController.swift
+++ iGenFamilyTree/iGenFamilyTree/TableViewController.swift
@@ -8,6 +8,40 @@
 
 import UIKit
 
+enum QuestionType: Int {
+    case brother = 0
+    case sister = 1
+    case sons = 2
+    case daughters = 3
+    case brotherMother = 4
+    case sisterMother = 5
+    case brotherFather = 6
+    case sisterFather = 7
+    
+    
+    func selectQuestion() -> String {
+        
+        switch self {
+        case .brother:
+            return NSLocalizedString("numberBrothers", comment: "")
+        case .sister:
+            return NSLocalizedString("numberSisters", comment: "")
+        case .sons:
+            return NSLocalizedString("numberSons", comment: "")
+        case .daughters:
+            return NSLocalizedString("numberDaughters", comment: "")
+        case .brotherMother:
+            return NSLocalizedString("numberBrotherfromMother", comment: "")
+        case .sisterMother:
+            return NSLocalizedString("numberSistersfromMother", comment: "")
+        case .brotherFather:
+            return NSLocalizedString("numberBrothersfromFather", comment: "")
+        case .sisterFather:
+            return NSLocalizedString("numberSistersfromFather", comment: "")
+        }
+    }
+}
+
 class TableViewController: UITableViewController {
     var numberOfSiblings:Int = 0
     var numberOfParents:Int = 0
